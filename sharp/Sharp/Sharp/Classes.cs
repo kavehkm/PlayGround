4257aa01ea4fd5d0fc1cdb6d1c0ffd5aa8add9bd
kavehkm <mehrbaniankaveh@gmail.com>
2021-09-25 17:03:28 +0330

    add classes

---
+++ sharp/Sharp/Sharp/Classes.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Sharp
+{
+    class Account
+    {
+        public int id;
+        public string firstName;
+        public string lastName;
+        private int balance;
+
+        public Account(int id, string firstName, string lastName, int balance = 0)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.balance = balance;
+        }
+
+        public int GetBalance()
+        {
+            return balance;
+        }
+
+        public void Variz(int value)
+        {
+            if (value < 0)
+            {
+                throw new Exception("Invalid value");
+            }
+            balance += value;
+        }
+
+        public void Bardasht(int value)
+        {
+            if (value < 0)
+            {
+                throw new Exception("Invalid value");
+            }
+
+            // compute remain
+            int remain = balance - value;
+
+            if (remain < 0)
+            {
+                throw new Exception("Not enough balance");
+            }
+
+            balance = remain;
+        }
+    }
+
+    class Account1
+    {
+        public int id;
+        public string firstName;
+        public string lastName;
+        private int balance;
+
+        public Account1(int id, string firstName, string lastName, int balance = 0)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.balance = balance;
+        }
+
+        public int Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Invalid value");
+                }
+                balance = value;
+            }
+        }
+    }
+
+    class Animal
+    {
+        public string name;
+
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Animal Sound");
+        }
+
+        public virtual void Sleep()
+        {
+            Console.WriteLine("ZzzzZzzz");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public string breed;
+
+        public Dog(string name, string breed) : base(name)
+        {
+            this.name = name;
+            this.breed = breed;
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine("Hop Hop Bow Bows");
+        }
+
+        public override void Sleep()
+        {
+            base.Sleep();
+            Console.WriteLine("WWwwwWW...");
+        }
+    }
+}
--- sharp/Sharp/Sharp/Program.cs
+++ sharp/Sharp/Sharp/Program.cs
@@ -30,6 +30,17 @@ namespace Sharp
             Array.Define();
             Array.DefineAndInitialize();
             Array.Access();
+
+            // classes
+            Account account1 = new Account(1, "kaveh", "mehrbanian", 100);
+            Console.WriteLine($"account1: {account1.id}, {account1.firstName}, {account1.lastName}, {account1.GetBalance()}");
+            account1.Variz(1000);
+            account1.Bardasht(600);
+            Console.WriteLine($"accont1's balance after operations is {account1.GetBalance()}");
+            // - working with properties
+            Account1 account2 = new Account1(2, "mehran", "falahaty");
+            account2.Balance = 1000;
+            Console.WriteLine(account2.Balance);
         }
     }
 }
